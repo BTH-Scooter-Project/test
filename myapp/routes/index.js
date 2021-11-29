999b2cd1d34813c20799d625b7f3a1ea61176aa0
Orkanen <Filip.v.antonsson@gmail.com>
2021-11-29 14:41:48 +0100

    v1.08
    Added login functionality under
    /contact

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,15 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title:"Express"
+    });
 });
 
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Express' });
 });
 
+router.post('/contact', function(req, res) {
+  let email = req.body.email;
+  let password = req.body.password;
+  console.log("test");
+  if(email=="Test@home.se" && password=="test") {
+    res.redirect('/features');
+  } else {
+    res.redirect('/');
+  }
+  //res.send( `Email: ${email} Password: ${password}`);
+});
+
 router.get('/features', function(req, res, next) {
-  res.render('features', { title: 'Features'});
+  res.render('features', { title: 'Express'});
 });
 
 module.exports = router;
