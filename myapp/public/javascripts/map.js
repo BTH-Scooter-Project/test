c6918bc29ebb0f81e6cf5a1ee8e20bd56a901bca
Orkanen <Filip.v.antonsson@gmail.com>
2021-12-01 17:15:12 +0100

    v1.12
    HTML5 geolocation implemented.
    flyTo current location using navigator.geolocation.watchPosition().
    It will update regularly!!
    Can later be used to put markers.
    ---Possible complications if user wants to look outside map range. ---

--- myapp/public/javascripts/map.js
+++ myapp/public/javascripts/map.js
@@ -1,26 +1,38 @@
+var map = null;
 //CREATING MAP FUNCTION
-function map(coords=[56.181932, 15.590525]) {
-  var map = L.map('map',{ center: coords, zoom: 20});
+function crtmap(coords=[56.181932, 15.590525]) {
+  map = L.map('map').setView(coords, 20);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{attribution: `&copy; contributors`}).addTo(map);
-  return map;
 }
 
 //GET COORDINATES FROM DEVICE
 function getPosition() {
     return new Promise((res, rej) => {
-    	navigator.geolocation.getCurrentPosition(res, rej)
+    	navigator.geolocation.watchPosition(res, rej)
     });
 }
 
 //THIS WILL GIVE ME THE USERS CURRENT COORDINATES
 //COORDINATES WILL THEN BE USED TO ESTABLISH THE MAP
 async function getLocation() {
+    var temp = [56.181932, 15.590525];
     var crd = await getPosition();
-    var map = L.map('map',{ center: [crd.coords.latitude, crd.coords.longitude], zoom: 20});
+    temp = [crd.coords.latitude, crd.coords.longitude];
+    var map = L.map('map',{ center: temp, zoom: 20});
 
     console.log(`[${crd.coords.latitude}, ${crd.coords.longitude}]`);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{attribution: `&copy; contributors`}).addTo(map);
-    return map;
+}
+
+//NOW FLYING TO CURRENT POSITION. !!MIGHT CAUSE ISSUES!!
+function success(pos){
+    var crd = pos.coords;
+    map.flyTo([crd.latitude, crd.longitude]);
+}
+
+function test() {
+    navigator.geolocation.watchPosition(success);
+    crtmap();
 }
